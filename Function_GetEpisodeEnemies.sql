7b1e488fe13762723424832501d0e27888890709
Rayyan-Asia <RayyanTawfieg@Gmail.com>
2023-04-15 14:44:58 +0300

    fnEnemies function is added, it accepts episodeId as a parameter and uses a join to retrieve the names of enemies in the episode

---
+++ Function_GetEpisodeEnemies.sql
@@ -0,0 +1,16 @@
+create function DoctorWho.fnEnemies (@EpisodeId INT)
+returns varchar(MAX) as
+begin
+	Declare @episodeEnemies varchar(MAX) ='';
+	Select  @episodeEnemies = @episodeEnemies + e.EnemyName + ', '
+	from Enemy e
+	JOIN EpisodeEnemy ee ON e.EnemyId = ee.EnemyId 
+	where ee.EpisodeId = @EpisodeId
+	
+	-- to remove any commas at the end
+	IF LEN(@episodeEnemies) > 0
+    SET @episodeEnemies = LEFT(@episodeEnemies, LEN(@episodeEnemies) - 2);
+	
+	return @episodeEnemies
+end;
+
